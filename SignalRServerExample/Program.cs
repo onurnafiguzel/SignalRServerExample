867fca2ff4b2972c05851c4f34e79938a263e5d3
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-07-22 21:36:35 +0300

    İlk deneme başarılı. 4 adet instance üerinde denendi.

--- SignalRServerExample/Program.cs
+++ SignalRServerExample/Program.cs
@@ -2,8 +2,15 @@ using SignalRServerExample.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+    policy.AllowAnyMethod()
+          .AllowAnyHeader()
+          .AllowCredentials()
+          .SetIsOriginAllowed(origin => true)
+));
 
 var app = builder.Build();
+app.UseCors();
 app.MapGet("/", () => "Hello World!");
 app.MapHub<MyHub>("/myhub");
 
